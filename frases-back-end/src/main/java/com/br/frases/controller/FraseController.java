485add41f1443b53ca073ec55fcd8c31e651d790
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2025-01-04 21:28:35 -0300

    Update FraseController.java

--- frases-back-end/src/main/java/com/br/frases/controller/FraseController.java
+++ frases-back-end/src/main/java/com/br/frases/controller/FraseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/series")
+@RequestMapping("/filmes-series")
 public class FraseController {
 
     @Autowired
